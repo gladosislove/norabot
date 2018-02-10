c395cc466a508969b3f387bc3c18c982defbb42b
gladosislove <33029439+gladosislove@users.noreply.github.com>
2018-02-10 15:50:58 +0000

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ bot.on('ready', () => {
   bot.on('message', (message) => {
     if (message.content === '!rules12345') {
         message.delete()
-        message.send('**Hello. Welcome to Sigma.**')
+        message.channel.send('**Hello. Welcome to Sigma.**')
     }
   });
 
