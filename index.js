37b777ee8c21855379bb81001e1cdddf44a17ec1
gladosislove <33029439+gladosislove@users.noreply.github.com>
2018-12-15 14:26:10 +0000

    Update index.js

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ bot.on('ready', () => {
     bot.user.setGame('Sigma') //
   });
 
-  List<String> lines = new String(Files.readAllBytes(Paths.get(rules.txt)));
+  String lines = new String(Files.readAllBytes(Paths.get(rules.txt)));
 
   bot.on('message', (message) => {
     if (message.content === '!rules12345') {
         message.delete()
-        message.channel.send(content)
+        message.channel.send(lines)
     }
   });
 
