9cffcd9aeafda71cb0508ca7d3b71aa91cece400
gladosislove <33029439+gladosislove@users.noreply.github.com>
2019-05-18 13:01:55 +0100

    Update index.js

--- index.js
+++ index.js
@@ -8,16 +8,30 @@ bot.on('message', async message => {
 
     if(message.content.startsWith(PREFIX + 'resource create')) {
 
+        //this part only allows the original message author to add to the resource and splits the title from the command
+
+        const filter = m => m.author.id === message.author.id;
         let cont = message.content.slice(PREFIX.length).split(' ');
         let args = cont.slice(1);
         let title = args.slice(1).join(' ');
 
+        message.delete()
+
+        //if the user hasn't entered a title, leave
+
         if(!title) {
-            message.channel.send('Please add a title to the resource before creating it.')
+            return message.channel.send('Please add a title to the resource before creating it.');
         }
 
-        message.delete()
-        message.channel.send(title)
+        message.channel.send('Please enter a description for this resource.').then(() => {
+            message.channel.awaitMessages(filter, {max: 1, time: 25000}).then(collected => {
+
+                let description = collected.content
+                collected.delete()
+                message.channel.send(description)
+            
+            });
+        });
 
     };
 
