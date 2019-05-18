957046d505bb13f2ad9c2d5b643827631899af06
gladosislove <33029439+gladosislove@users.noreply.github.com>
2019-05-18 11:56:47 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,72 +2,32 @@ const Discord = require('discord.js');
 const bot = new Discord.Client();
 const PREFIX = '.'
 
-// misc arrays
-
-const flip = ['you flipped a coin, it landed on heads.', 'you flipped a coin, it landed on tails.'];
-
-const eightball = [
-    'maybe.', 'nah.', 'I hope so.', 'not today, friend.',
-    'perhaps.', 'I think so.', 'I hope not.',
-    'I hope so.', 'never!', 'forget about it.', 'really?',
-    'sorry, friend.', 'hell yeah.', 'hell no.', 'don\'t get your hopes up',
-    'uh... I guess?', 'maybe? Maybe not. It\'s hard to tell.', 'yes..? Maybe?'
-];
-
-bot.on('ready', () => {
-    bot.user.setGame('your heart <3')
-    console.log('Online.')
-});
-
-bot.on('guildMemberAdd', member => {
-    var joinRole = member.guild.roles.find('name', 'members');
-    member.addRole(joinRole)
-});
+//resource creator
 
 bot.on('message', async message => {
 
-    // avoiding bot spam
+    if(message.contentstartsWith(PREFIX + 'resource')) {
 
-    if(message.author.bot) return;
+        let userResource = message.content.split(' ').slice(1).join(' ');
 
-    // flirt command
-
-    if (message.content.startsWith(PREFIX + 'flirt')) {
-        message.reply(romance[Math.floor(Math.random()*romance.length)])
-    }
-
-    // coin flip command
-
-    if (message.content.startsWith(PREFIX + 'coinflip')) {
-        message.reply(flip[Math.floor(Math.random()*flip.length)])
-    }
-
-    // 8ball command
-
-    if (message.content.startsWith(PREFIX + '8ball')) {
-        let cont = message.content.slice(PREFIX.length).split(' ');
-        let args = cont.slice(1);
-
-        if(!args[0]) return message.reply('please ask a full question.');
-
-        message.reply(eightball[Math.floor(Math.random()*eightball.length)])
-    }
-
-    // rules command
-    
-    if (message.content.startsWith(PREFIX + 'rules')) {
-        if (!message.member.roles.find('name', 'thot supreme')) {
+        if (!message.content.find(', ')) {
+            message.channel.send('Hold on a second! Please separate your fields using the "," key.');
             return;
         }
-        
-        message.delete()
 
-        var fs = require("fs");
-        var rules = fs.readFileSync("rules.txt", {"encoding": "utf-8"});
-        message.channel.send(rules)
-        
-    }
+        let userArray = userResource.split(', ')
+            
 
+        let embed = new Discord.RichEmbed()
+                    .setTitle('New Resource')
+                    .setDescription(`Added by @${message.author.tag}`)
+                    .setColor('#ffd63c')
+                    .addField(userArray[1], userArray[2], true)
+                    .setTimestamp();
+                
+        bot.channels.get('578993596544450561').send({embed});
+
+    };
 
 });
 
